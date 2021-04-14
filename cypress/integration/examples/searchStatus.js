87781e3e0d58d61c705aaf665a4b7a1ae4eaf9ac
fypnlp <39129689+fypnlp@users.noreply.github.com>
2021-04-14 06:14:27 +0100

    Update searchStatus.js

--- cypress/integration/examples/searchStatus.js
+++ cypress/integration/examples/searchStatus.js
@@ -1,10 +1,10 @@
 // <reference types="Cypress" />
 
-describe('Find Applicants location', () => {
+describe('Applicants Status', () => {
     beforeEach(() => {
       cy.visit("http://localhost:3000/")
     })
-    it('Find city with lowercase letter', () => {
+    it('Move Applicants Status', () => {
       cy.get(`.CrewMember-container[data-cy="6fc9d2bb-590c-4351-b0b9-45686e61a4ed"] .CrewMember-up`).click()
       cy.get(`.CrewMember-container[data-cy="6fc9d2bb-590c-4351-b0b9-45686e61a4ed"] .CrewMember-down`).click()
     })
